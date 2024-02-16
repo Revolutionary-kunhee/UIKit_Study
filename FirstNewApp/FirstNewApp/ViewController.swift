818b6f55822778ca30dfd106d98f9dbf8df3344b
Revolutionary-kunhee <vivakunhee@gmail.com>
2024-02-16 16:44:33 +0900

    활용 앱 1 화면 설정

---
+++ FirstNewApp/FirstNewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstNewApp
+//
+//  Created by 건희 on 2/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+ 
